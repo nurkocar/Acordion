4ed5ec343ffa82e7233b723ac23292f93ef75254
nurkocar <fatmanurkocar61@gmail.comi>
2021-06-26 20:30:39 +0100

    Created dummy datas for acordion and faq module

---
+++ src/data/acordionData.js
@@ -0,0 +1,32 @@
+export const acordionData = [
+  {
+    "title": "High-touch Customer Support.",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    "icon": 'customer'
+  },
+  {
+    "title": "Security & Hosting.",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    "icon": 'security'
+  },
+  {
+    "title": "Unlimited Plugins & Storage.",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    "icon": 'storage'
+  },
+  {
+    "title": "Time-Saving Integrations.",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    "icon": 'time'
+  },
+  {
+    "title": "Tailored Learning Experiences.",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    "icon": 'learning'
+  },
+  {
+    "title": "World-Class Support and Industry-Leading Solutions.",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    "icon": 'world'
+  }
+];
---
+++ src/data/dummyData.js
@@ -0,0 +1,8 @@
+export const dummyData = [
+  {
+    title: "Open LMS EDU: A Total K-12 eLearning Solution",
+    text: "Open LMS provides K-12 institutions with a fully-managed Open LMS EDU or Moodle™ experience to make meeting eLearning goals easy. Our team of experts take care of every aspect of your LMS instance so you can focus on providing engaging and effective digital education to your learners.",
+    buttonText: "Request a demo"
+  }
+];
+
