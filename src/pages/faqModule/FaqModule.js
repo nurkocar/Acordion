33ad067c8326eb517f798428ff555b1ea7a622c6
nurkocar <fatmanurkocar61@gmail.comi>
2021-06-26 22:41:08 +0100

    Created Faq module and added to App.js

---
+++ src/pages/faqModule/FaqModule.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { Title } from "../../components/title";
+import { Subtitle } from "../../components/subtitle";
+import { Acordion } from "../../components/acordion";
+import { Icon } from '../../components/icon';
+import { Button } from "../../components/button";
+
+import styles from './FaqModule.module.css';
+
+export const FaqModule = ({ data, iconPosition }) => {
+
+    const [icon, setIcon] = useState('customer');
+
+    return (
+        <div>
+            {data.map((item, index) => (
+                <div key={index}>
+                    <Title title={item.title} />
+                    <Subtitle subtitle={item.text} />
+                    <div className={`${styles.acordionAndIconContainer} ${iconPosition === 'left' ? styles.iconLeft : ''}`} >
+                        <Acordion setIcon={setIcon} />
+                        <Icon icon={icon} />
+                    </div>
+                    <Button title={item.buttonText} />
+                </div>
+            ))}
+
+        </div>
+    )
+};
