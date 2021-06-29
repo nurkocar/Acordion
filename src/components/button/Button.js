8b264f223084a0d63f342600a555a86eff544437
nurkocar <fatmanurkocar61@gmail.comi>
2021-06-30 00:12:57 +0100

    handled button onclick null for now

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -3,7 +3,7 @@ import styles from './Button.module.css';
 export const Button = ({ title }) => {
     return (
         <div className={styles.buttonContainer}>
-            <button className={styles.button}>{title}</button>
+            <button className={styles.button} onClick={() => null}>{title}</button>
         </div>
     )
 }
